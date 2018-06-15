ef17d7a38df058437d9bfe02e6e0f79d12a2b0da
nan.zhou <nan.zhou@baifendian.com>
2018-06-15 15:39:46 +0800

    代码提交

--- easydp-web/src/main/java/com/freedom/easydp/StartApplication.java
+++ easydp-web/src/main/java/com/freedom/easydp/StartApplication.java
@@ -14,8 +14,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class StartApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(StartApplication.class, args);
+  public static void main(String[] args1) {
+    SpringApplication.run(StartApplication.class, args1);
   }
 
 }
